0ad0df73e084911908ca4411c13b12f79a08014f
sonamtaa <64897638+sonamtaa@users.noreply.github.com>
2023-09-04 16:27:05 +0600

    Add graphql

--- app/graphql/resolvers/cars.rb
+++ app/graphql/resolvers/cars.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 module Resolvers
-  class ActivityLogs < GraphQL::Schema::Resolver
-    description 'Fetches activity log for given ticket ID'
+  class Cars < GraphQL::Schema::Resolver
+    description 'Fetches Cars from API'
 
-    type Types::ActivityLog.connection_type, null: true
+    type Types::Cars.connection_type, null: true
 
-    argument :skip, Integer, 'Used to offset the records', required: false
-    argument :ticket_id, ID, 'ID of ticket', required: false
+    # argument :skip, Integer, 'Used to offset the records', required: false
+    # argument :ticket_id, ID, 'ID of ticket', required: false
 
     def resolve(**args)
-      activity_logs = ActivityLog.where(loggable_id: args[:ticket_id], loggable_type: 'Ticket').order(created_at: :desc)
-      args[:skip] ? activity_logs.offset(args[:skip]) : activity_logs
+      cars = Car.all.order(created_at: :desc)
+      # args[:skip] ? cars.offset(args[:skip]) : cars
+      cars
     end
   end
 end
